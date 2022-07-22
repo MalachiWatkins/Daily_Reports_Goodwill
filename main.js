c0faf7448da5e848c57655e35045c0e980c30ddd
malachi.watkins3535@gmail.com <malachi.watkins3535@gmail.com>
2022-07-22 13:11:57 -0500

    WIP

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const config = {
 async function url() {
   var i = 1;
   let res_list = [];
-  while (i < 60) {
+  while (i < 3) {
 
     const url = 'https://app.uprightlabs.com/api/orders?page=' + i.toString() + '&per_page=40&sort=ordered_at.desc'
     const result = await data(url)
@@ -66,7 +66,7 @@ async function parse(date) {
 
             var order_dict = {};
             var store_number_order_dict = {};
-            order_dict[market] = item['total'];
+            order_dict[String(market)] = item['total'];
             store_number_order_dict[item['store_name']] = order_dict;
             subtotal.push(store_number_order_dict)
           });
@@ -83,22 +83,24 @@ async function parse(date) {
   }
   return new Promise((resolve, reject) => {
     var nested_data = [subtotal, shipping]
-    resolve(nested_data)
+    resolve(nested_data) // data here is nested and the subtotals have store numbers as keys ex: {store_num: {sgw: '10.99'}}
   });
-  // console.log(subtotal.length);
-  // console.log(shipping);
+
 }
 
 
 async function sales_data() {
-  const data = await parse('2022-07-21')
+  const data = await parse('2022-07-22')
   var subtotal = data[0]
   var sub_price = []
+  var sgw_sub_price = []
+  var ebay_sub_price = []
+
   var shipping = data[1]
   var ship_price = []
   shipping.forEach((single_shipping) => {
     for (var key in single_shipping) {
-      ship_price.push(parseInt(single_shipping[key]))
+      ship_price.push(parseFloat(single_shipping[key]))
     }
   });
 
@@ -106,28 +108,49 @@ async function sales_data() {
   subtotal.forEach((store) => {
     for (var key in store) {
       var price = store[key];
+      // console.log(price);
+      if (price['shopgoodwill']) {
+        sgw_sub_price.push(parseFloat(price['shopgoodwill']))
+      } else {
+        ebay_sub_price.push(parseFloat(price['ebay']))
+      }
       for (var key in price) {
-        sub_price.push(parseInt(price[key]))
+
+        sub_price.push(parseFloat(price[key]))
       }
     }
 
   });
+
   var sum = sub_price.reduce(function(a, b) {
     return a + b;
   }, 0);
   var total_sales = sum
 
-
   var sum_shipping = ship_price.reduce(function(a, b) {
     return a + b;
   }, 0);
   var total_shipping = sum_shipping
+
+  var sgw_sum = sgw_sub_price.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  var total_sgw = sgw_sum
+
+  var ebay_sum = ebay_sub_price.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  var total_ebay = ebay_sum
+
   var items_sold = subtotal.length
   var ppl = total_sales / items_sold
-  var roundedppl = ppl.toFixed(1);
-  console.log('Total Sales: ' + total_sales);
-  console.log('Total Shipping Revenue: ' + total_shipping);
-  console.log('Items Sold: ' + items_sold);
+  var roundedppl = ppl.toFixed(2);
+  // ONCE DONE CREATE A PROMISE HERE FOR AUTO FILL FUNCTION
+  console.log('Total Sales: ' + total_sales.toFixed(2));
+  console.log('Ebay Sales: ' + total_ebay.toFixed(2));
+  console.log('SGW Sales: ' + total_sgw.toFixed(2));
+  console.log('Total Shipping Revenue: ' + total_shipping.toFixed(2));
+  console.log('Items Sold: ' + items_sold.toFixed(2));
   console.log('Daily PPL: ' + roundedppl);
 
 }
