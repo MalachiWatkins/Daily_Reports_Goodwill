47d8c8b606b59014edaa73cb8073182e48fb7649
malachi.watkins3535@gmail.com <malachi.watkins3535@gmail.com>
2022-07-24 17:21:36 -0500

    WIP

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const json = require('json');
 var lodash = require('lodash');
 const API_TOKEN = ''
 
+// Headers Config
 const config = {
   headers: {
     'X-Authorization': API_TOKEN,
@@ -11,28 +12,28 @@ const config = {
 };
 
 // // TODO: Clean  up code, start next listing section
-async function url() {
-  var i = 1;
-  let res_list = [];
-  while (i < 3) {
 
-    const url = 'https://app.uprightlabs.com/api/orders?page=' + i.toString() + '&per_page=40&sort=ordered_at.desc'
+// url search
+let max_page = 3
+async function url() {
+  var page_start = 1; // Page Start
+  let res_list = []; // Response List
+  while (page_start < max_page) {
+    const url = 'https://app.uprightlabs.com/api/orders?page=' + page_start.toString() + '&per_page=40&sort=ordered_at.desc'
     const result = await data(url)
     res_list.push(result)
-    i++
+    page_start++
   }
   return new Promise((resolve, reject) => {
     resolve(res_list)
   });
-
 }
 
+// response function
 function data(url) {
   return new Promise((resolve, reject) => {
     axios
-      // search 150 Pages Max 100
       .get(url, config)
-
       .then(res => {
         resolve(res)
       });
@@ -51,22 +52,18 @@ async function parse(date) {
     for (var i = 0; i < single_data['orders'].length; i++) {
       var order = single_data['orders']
       var single_order = order[i]
-
-
       var paid_at = single_order['paid_at']
       try {
         if (paid_at.includes(date)) {
           if (single_order['shipped_at'] != null) {
             shipped_orders.push(single_order['shipped_at'])
           }
-
           var market = single_order['market_name']
           var ship_total = single_order['shipping_total']
           var ship_dict = {}
           ship_dict[market] = ship_total;
           shipping.push(ship_dict)
           single_order['order_items'].forEach((item, i) => {
-
             var order_dict = {};
             var store_number_order_dict = {};
             order_dict[String(market)] = item['total'];
@@ -79,18 +76,14 @@ async function parse(date) {
       } finally {
         var n = 'null'
       }
-
     }
-
     x++
   }
   return new Promise((resolve, reject) => {
     var nested_data = [subtotal, shipping, shipped_orders]
     resolve(nested_data) // data here is nested and the subtotals have store numbers as keys ex: {store_num: {sgw: '10.99'}}
   });
-
 }
-
 
 async function sales_data() {
   const data = await parse('2022-07-22')
@@ -106,49 +99,27 @@ async function sales_data() {
       ship_price.push(parseFloat(single_shipping[key]))
     }
   });
-
-  // Totals
   subtotal.forEach((store) => {
     for (var key in store) {
       var price = store[key];
-      // console.log(price);
       if (price['shopgoodwill']) {
         sgw_sub_price.push(parseFloat(price['shopgoodwill']))
       } else {
         ebay_sub_price.push(parseFloat(price['ebay']))
       }
       for (var key in price) {
-
         sub_price.push(parseFloat(price[key]))
       }
     }
-
   });
-
-
-  var sum = sub_price.reduce(function(a, b) {
-    return a + b;
-  }, 0);
-  var total_sales = sum
-
-  var sum_shipping = ship_price.reduce(function(a, b) {
-    return a + b;
-  }, 0);
-  var total_shipping = sum_shipping
-
-  var sgw_sum = sgw_sub_price.reduce(function(a, b) {
-    return a + b;
-  }, 0);
-  var total_sgw = sgw_sum
-
-  var ebay_sum = ebay_sub_price.reduce(function(a, b) {
-    return a + b;
-  }, 0);
-  var total_ebay = ebay_sum
-
+  var total_sales = lodash.sum(sub_price);
+  var total_shipping = lodash.sum(ship_price);
+  var total_sgw = lodash.sum(sgw_sub_price);
+  var total_ebay = lodash.sum(ebay_sub_price);
+  var roundedppl = ppl.toFixed(2);
   var items_sold = subtotal.length
   var ppl = total_sales / items_sold
-  var roundedppl = ppl.toFixed(2);
+
   // ONCE DONE CREATE A PROMISE HERE FOR AUTO FILL FUNCTION
   console.log('Shipped Orders: ' + shipped_order.length);
   console.log('Total Sales: ' + total_sales.toFixed(2));
@@ -159,4 +130,6 @@ async function sales_data() {
   console.log('Daily PPL: ' + roundedppl);
 
 }
+// This is for [Shipped Orders, Total Sales, Ebay/sgw sales, Shipping rev, Items sold, daily ppl] *daily only
+
 sales_data()
