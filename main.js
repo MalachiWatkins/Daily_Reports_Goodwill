c1072bd14fa8860bafb3b92f7b25d267b026cc4f
malachi.watkins3535@gmail.com <malachi.watkins3535@gmail.com>
2022-07-26 13:30:50 -0500

    WIP

--- main.js
+++ main.js
@@ -3,11 +3,7 @@ const axios = require('axios');
 const json = require('json');
 var lodash = require('lodash');
 const API_TOKEN = ''
-var max_page = 50 // max page
-var page_start = 1; // Page Start
-const order_url = 'https://app.uprightlabs.com/api/orders?page=' + page_start.toString() + '&per_page=40&sort=ordered_at.desc'
-const listing_url = 'https://app.uprightlabs.com/api/reports/productivity/user?time_start=2022-07-18&time_end=2022-07-18&hide_inactive_users=true'
-// Headers Config
+// test
 const config = {
   headers: {
     'X-Authorization': API_TOKEN,
@@ -15,57 +11,63 @@ const config = {
 };
 
 // // TODO: Clean  up code, start next listing section
+async function url() {
+  var i = 1;
+  let res_list = [];
+  while (i < 10) {
 
-// url search
-async function url_response(url) {
-
-  let res_list = []; // Response List
-  while (page_start < max_page) {
+    const url = 'https://app.uprightlabs.com/api/orders?page=' + i.toString() + '&per_page=40&sort=ordered_at.desc'
     const result = await data(url)
     res_list.push(result)
-    page_start++
+    i++
   }
   return new Promise((resolve, reject) => {
     resolve(res_list)
   });
+
 }
 
-// response function
 function data(url) {
   return new Promise((resolve, reject) => {
     axios
+      // search 150 Pages Max 100
       .get(url, config)
+
       .then(res => {
         resolve(res)
       });
   });
 }
 
-async function parse(date, url) {
-  const response = await url_response(url)
+async function parse(date) {
+  const response = await url()
   var x = 0;
   let subtotal = []
   let shipping = []
   let shipped_orders = []
   while (x < response.length) {
+
     var singel_res = response[x]
     var single_data = singel_res['data']
     for (var i = 0; i < single_data['orders'].length; i++) {
       var order = single_data['orders']
       var single_order = order[i]
+
+
       var paid_at = single_order['paid_at']
       try {
         if (paid_at.includes(date)) {
           if (single_order['shipped_at'] != null) {
-            console.log(single_order['shipped_at']);
             shipped_orders.push(single_order['shipped_at'])
           }
+
           var market = single_order['market_name']
           var ship_total = single_order['shipping_total']
           var ship_dict = {}
           ship_dict[market] = ship_total;
           shipping.push(ship_dict)
           single_order['order_items'].forEach((item, i) => {
+
             var order_dict = {};
             var store_number_order_dict = {};
             order_dict[String(market)] = item['total'];
@@ -78,21 +80,27 @@ async function parse(date, url) {
       } finally {
         var n = 'null'
       }
+
     }
+
     x++
   }
   return new Promise((resolve, reject) => {
     var nested_data = [subtotal, shipping, shipped_orders]
     resolve(nested_data) // data here is nested and the subtotals have store numbers as keys ex: {store_num: {sgw: '10.99'}}
   });
+
 }
-async function sales_data(date, url) {
-  const data = await parse(date, url)
+
+
+async function sales_data() {
+  const data = await parse('2022-07-25')
   var subtotal = data[0]
   var sub_price = []
   var sgw_sub_price = []
   var ebay_sub_price = []
   var shipped_order = data[2]
+  console.log(shipped_order);
   var shipping = data[1]
   var ship_price = []
   shipping.forEach((single_shipping) => {
@@ -100,29 +108,51 @@ async function sales_data(date, url) {
       ship_price.push(parseFloat(single_shipping[key]))
     }
   });
+
+  // Totals
   subtotal.forEach((store) => {
     for (var key in store) {
       var price = store[key];
+      // console.log(price);
       if (price['shopgoodwill']) {
         sgw_sub_price.push(parseFloat(price['shopgoodwill']))
       } else {
         ebay_sub_price.push(parseFloat(price['ebay']))
       }
       for (var key in price) {
+
         sub_price.push(parseFloat(price[key]))
       }
     }
+
   });
-  var total_sales = lodash.sum(sub_price);
-  var total_shipping = lodash.sum(ship_price);
-  var total_sgw = lodash.sum(sgw_sub_price);
-  var total_ebay = lodash.sum(ebay_sub_price);
+
+
+  var sum = sub_price.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  var total_sales = sum
+
+  var sum_shipping = ship_price.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  var total_shipping = sum_shipping
+
+  var sgw_sum = sgw_sub_price.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  var total_sgw = sgw_sum
+
+  var ebay_sum = ebay_sub_price.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  var total_ebay = ebay_sum
+
+  var items_sold = subtotal.length
   var ppl = total_sales / items_sold
   var roundedppl = ppl.toFixed(2);
-  var items_sold = subtotal.length
-
-
   // ONCE DONE CREATE A PROMISE HERE FOR AUTO FILL FUNCTION
+  console.log('---------------------------- Daily ------------------------');
   console.log('Shipped Orders: ' + shipped_order.length);
   console.log('Total Sales: ' + total_sales.toFixed(2));
   console.log('Ebay Sales: ' + total_ebay.toFixed(2));
@@ -130,11 +160,11 @@ async function sales_data(date, url) {
   console.log('Total Shipping Revenue: ' + total_shipping.toFixed(2));
   console.log('Items Sold: ' + items_sold.toFixed(2));
   console.log('Daily PPL: ' + roundedppl);
+  console.log('------------------------------------------------------------');
+
 
 }
-// This is for [Shipped Orders, Total Sales, Ebay/sgw sales, Shipping rev, Items sold, daily ppl] *daily only
-
-
+sales_data()
 async function listing(url) {
   return new Promise((resolve, reject) => {
     var list = []
@@ -152,18 +182,4 @@ async function listing(url) {
         resolve(res)
       });
   });
-
-
 }
-listing(listing_url);
-sales_data('2022-07-18', order_url)
-// This is what pops up now NEEDS fixed its with the sales _data function IDK whats goin on
-// Daily Postings: 320
-// Avg Posting/Lister: 64
-// Shipped Orders: 0
-// Total Sales: 0.00
-// Ebay Sales: 0.00
-// SGW Sales: 0.00
-// Total Shipping Revenue: 0.00
-// Items Sold: 0.00
-// Daily PPL: NaN
