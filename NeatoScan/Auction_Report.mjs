eee5fb0580db2258b5b896251aca94ae9e17eb83
malachi.watkins3535@gmail.com <malachi.watkins3535@gmail.com>
2022-09-19 11:34:01 -0500

    WIP

--- NeatoScan/Auction_Report.mjs
+++ NeatoScan/Auction_Report.mjs
@@ -22,41 +22,27 @@ const soapRequest = require('easy-soap-request');
 var fs = require('fs'),
     parseString = require("xml2js").parseString,
     xml2js = require("xml2js");
-<<<<<<< HEAD
 const { JSDOM } = require("jsdom");
 const { window } = new JSDOM();
 var start = window.performance.now();
-=======
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
 
 // XML File Locations Put this in config file
 var Request_Auction_Order = '/Users/malachiwatkins/Desktop/work/Daily_Reports_Goodwill/NeatoScan/xml_files/Auction_order.xml'
 //'/home/malachi/Desktop/Projects/Daily_Reports_Goodwill/NeatoScan/xml_files/Auction_order.xml'
 var Request_Auction_Inv = '/Users/malachiwatkins/Desktop/work/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_inventory.xml'
 //'/home/malachi/Desktop/Projects/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_inventory.xml'
-<<<<<<< HEAD
 var Authenticate_xml = '/Users/malachiwatkins/Desktop/work/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_auth.xml'
 //'/home/malachi/Desktop/Projects/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_auth.xml'
 var Request_Auction_Activity = '/Users/malachiwatkins/Desktop/work/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_activity.xml'
 var Request_Auction_Container_details = '/Users/malachiwatkins/Desktop/work/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_container_details.xml'
 var Request_Auction_Refunds = '/Users/malachiwatkins/Desktop/work/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_refunds.xml'
                                                                                                              
-=======
-//
-
-var Authenticate_xml = '/Users/malachiwatkins/Desktop/work/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_auth.xml'
-//'/home/malachi/Desktop/Projects/Daily_Reports_Goodwill/NeatoScan/xml_files/auction_auth.xml'
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
 
 
 const url_auctions = SOAP_urls['Auction_Url']; // Main Book URL
 
 
-<<<<<<< HEAD
 // Auth Function
-=======
-// Request Function
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
 async function AuctionreqAuth(url, xml, CL, type) {
     const USER_Headers = {
         headers: {
@@ -89,22 +75,13 @@ async function AuctionreqAuth(url, xml, CL, type) {
 
                     });
                 }
-<<<<<<< HEAD
             });
-=======
-
-            });
-
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
         }).catch(err => {
             console.log(err)
         });
     });
 }
-<<<<<<< HEAD
 // Parses Auth Response Into Authentication Package
-=======
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
 async function authFIleparse() {
     return new Promise((resolve, reject) => {
         fs.readFile(Authenticate_xml, "utf-8", function (err, data) {
@@ -117,10 +94,7 @@ async function authFIleparse() {
                 var builder = new xml2js.Builder();
                 const xml = builder.buildObject(json);
                 const auth_response = await AuctionreqAuth(url_auctions, xml, 'Auctions_Report_Content_Len', 'auth')
-<<<<<<< HEAD
                 console.log('Authenticated!');
-=======
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
                 resolve(auth_response);
             });
 
@@ -128,7 +102,6 @@ async function authFIleparse() {
     });
 }
 
-<<<<<<< HEAD
 // // authFIleparse()
 // async function main_auth() {
 //     const authentication_package = await authFIleparse()
@@ -136,14 +109,6 @@ async function authFIleparse() {
 // }
 
 // Adds Authentication to XML Soap Envelope
-=======
-// authFIleparse()
-async function main_auth() {
-    const authentication_package = await authFIleparse()
-    console.log(authentication_package);
-}
-
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
 async function AuctionSoapEnv(SOAPENV, type) {
     return new Promise((resolve, reject) => {
         fs.readFile(SOAPENV, "utf-8", function (err, data) {
@@ -173,7 +138,6 @@ async function AuctionSoapEnv(SOAPENV, type) {
         });
     });
 }
-<<<<<<< HEAD
 // Request from SOAP Endpoint 
 async function AuctionRequest(Request_xml, type) {
     return new Promise(async (resolve, reject) => {
@@ -215,20 +179,20 @@ async function OrderAucion_parse() {
             addTime: [ '2022-09-08T12:31:00' ],
             startTime: [ '2022-09-08T12:31:00' ],
             endTime: [ '2022-09-14T03:43:00' ],
-            orderID: [ '47173' ],
-            orderItemID: [ '32173' ],
+            orderID: [ '' ],
+            orderItemID: [ '' ],
             externalOrderID: [ '38417210' ],
             externalOrderItemID: [ '151373648' ],
             createdDate: [ '2022-09-16T00:58:51' ],
             DateSold: [ '2022-09-16T00:58:51' ],
             DatePaid: [ '2022-09-16T01:00:34' ],
             fulfillmentLevel: [ 'Pickup' ],
-            addressLine1: [ '510 Kirkpatrick St' ],
+            addressLine1: [ '' ],
             addressLine2: [ '' ],
             addressLine3: [ '' ],
-            city: [ 'Odessa' ],
-            state: [ 'MO' ],
-            zip: [ '64076-1615' ],
+            city: [ '' ],
+            state: [ '' ],
+            zip: [ '' ],
             country: [ 'United States' ],
             salesAmount: [ '17.00' ],
             shippingActual: [ '0.00' ],
@@ -250,45 +214,6 @@ async function OrderAucion_parse() {
             orderItemStatusID: [ '3' ],
             orderItemStatus: [ 'Awaiting Pickup' ]
           }
-        // Put this in config
-        var ORDER_REPORT = {
-            'SGW' : {
-                'Shared Revenue/Ecommerce': {'sub': [], 'ship': []},
-                'Goodwill East 63rd St': {'sub': [], 'ship': []},
-                'Goodwill Lee’s Summit': {'sub': [], 'ship': []},
-                'Goodwill Blue Springs': {'sub': [], 'ship': []},
-                'Goodwill Manhattan': {'sub': [], 'ship': []},
-                'Goodwill Bonner Springs': {'sub': [], 'ship': []},
-                'Goodwill Leavenworth': {'sub': [], 'ship': []},
-                'Goodwill Overland Park': {'sub': [], 'ship': []},
-                'Goodwill Topeka': {'sub': [], 'ship': []},
-                'Goodwill Pittsburg': {'sub': [], 'ship': []},
-                'Goodwill Shawnee': {'sub': [], 'ship': []},
-                'Goodwill on North Oak': {'sub': [], 'ship': []},
-                'Goodwill Liberty': {'sub': [], 'ship': []},
-                'Goodwill Outlet Store': {'sub': [], 'ship': []},
-                'Goodwill Leawood Donation Center': {'sub': [], 'ship': []},
-                'Goodwill Lawrence': {'sub': [], 'ship': []}
-            },
-            'Ebay' : {
-                'Shared Revenue/Ecommerce': {'sub': [], 'ship': []},
-                'Goodwill East 63rd St': {'sub': [], 'ship': []},
-                'Goodwill Lee’s Summit': {'sub': [], 'ship': []},
-                'Goodwill Blue Springs': {'sub': [], 'ship': []},
-                'Goodwill Manhattan': {'sub': [], 'ship': []},
-                'Goodwill Bonner Springs': {'sub': [], 'ship': []},
-                'Goodwill Leavenworth': {'sub': [], 'ship': []},
-                'Goodwill Overland Park': {'sub': [], 'ship': []},
-                'Goodwill Topeka': {'sub': [], 'ship': []},
-                'Goodwill Pittsburg': {'sub': [], 'ship': []},
-                'Goodwill Shawnee': {'sub': [], 'ship': []},
-                'Goodwill on North Oak': {'sub': [], 'ship': []},
-                'Goodwill Liberty': {'sub': [], 'ship': []},
-                'Goodwill Outlet Store': {'sub': [], 'ship': []},
-                'Goodwill Leawood Donation Center': {'sub': [], 'ship': []},
-                'Goodwill Lawrence': {'sub': [], 'ship': []}
-            }
-        }
         var x = 0 
         while (x<orders.length) {
             var single_order_status = orders[x]['orderItemStatus']
@@ -322,61 +247,9 @@ async function OrderAucion_parse() {
             x++
             
         }
-        var total_sub_sgw = []
-        var total_ship_sgw = []
 
-       // var ppi = total_sales / times_sold
-        for (const [key, value] of Object.entries(ORDER_REPORT['SGW'])) {
-            value['sub'].forEach(subtotal => {
-                total_sub_sgw.push(parseFloat(subtotal))
-            });
-            value['ship'].forEach(shiptotal => {
-                total_ship_sgw.push(parseFloat(shiptotal))
-            });
-          }
-        var sub_total_sgw = total_sub_sgw.reduce(function(a, b) {
-            return a + b;
-        }, 0);
-        var ship_total_sgw = total_ship_sgw.reduce(function(a, b) {
-            return a + b;
-        }, 0);
-        var total_sub_ebay = []
-        var total_ship_sgw = []
 
-       // var ppi = total_sales / times_sold
-        for (const [key, value] of Object.entries(ORDER_REPORT['Ebay'])) {
-            value['sub'].forEach(subtotal => {
-                total_sub_ebay.push(parseFloat(subtotal))
-            });
-            value['ship'].forEach(shiptotal => {
-                total_ship_sgw.push(parseFloat(shiptotal))
-            });
-          }
-        var sub_total_ebay = total_sub_ebay.reduce(function(a, b) {
-            return a + b;
-        }, 0);
-        var ship_total_ebay = total_ship_sgw.reduce(function(a, b) {
-            return a + b;
-        }, 0);
-        var items_sold_SGW = total_sub_sgw.length
-        var items_sold_ebay = total_sub_ebay.length
-        var Total_sales = parseFloat(sub_total_sgw.toFixed(2)) + parseFloat(sub_total_ebay.toFixed(2))
-        var Total_Items_Sold = items_sold_SGW + items_sold_ebay
-        var Total_ppi = Total_sales / Total_Items_Sold
-        // THESE Need to be put in DB
-        var main_order_dict =  {
-            'Total_Items_Sold' : Total_Items_Sold,
-            'Total_Sales': Total_sales,
-            'Total_Shipping': parseFloat(ship_total_sgw.toFixed(2)) + parseFloat(ship_total_ebay.toFixed(2)),
-            'Total_PPI' : parseFloat(Total_ppi.toFixed(2)),
-            'SGW_Items_Sold' : items_sold_SGW,
-            'Total_SGW_Sales': parseFloat(sub_total_sgw.toFixed(2)),
-            'Total SGW_Shipping': parseFloat(ship_total_sgw.toFixed(2)),
-            'Ebay_Items_Sold': items_sold_ebay,
-            'Total_Ebay_Sales': parseFloat(sub_total_ebay.toFixed(2)),
-            'Total_Ebay_Shipping': parseFloat(ship_total_ebay.toFixed(2)),
-        }
-        console.log(main_order_dict);
+
     });
     
 
@@ -500,25 +373,3 @@ async function Auction_Refunds() {
 OrderAucion_parse() 
 var end = window.performance.now();
 console.log(`Execution time: ${end - start} ms`);
-=======
-async function AuctionRequest(Request_xml, type) {
-    // ALL THIS CAN BE CONDENCED DOWN TO ONE XML FILE A FEW KEYS NEED CHANGEING
-    const soap_data = await AuctionSoapEnv(Request_xml, type)
-    console.log('Request Envelope Ready');
-    // ALL That needs to be done is to change headers to pull from config and thats it
-    // Long Issue over GUID cuz me no smart sometimes and brain != workin
-    const USER_Headers = {
-        headers: {'User-Agent': USER_AGENT,
-        'Content-Type': SOAP_Header_CONTENT_TYPE['Auction_Content_Type'],'Host': SOAP_Header_Host_Config['Books_Host'], "Content-Length":SOAP_Content_len['Auctions_Report_Content_Len'], 
-        "Expect": "100-continue", "Connection": "Keep-Alive"}
-    }
-    axios.post(url_auctions, soap_data, USER_Headers).then(response => {
-            console.log(response);
-     })
-}
-
-AuctionRequest(Request_Auction_Order, 'Orders') //// Request_Auction_Inv, 'Inventory'
-async function Aucion_parse() {
-    
-}
->>>>>>> 24f68e75d20dfc88b5c6986f37e9a1b2872592b2
